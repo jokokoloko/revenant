b8299baf0c17ef727a58c24e5fee59e095a3fb52
jokokoloko <jhowland82@gmail.com>
2018-09-11 15:49:29 -0700

    Added FormLead.js

--- src/js/react/Home.js
+++ src/js/react/Home.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Basic from './section/Basic';
+import FormLead from './form/FormLead';
 
 const Home = () => (
     <main id="main" role="main">
         <div className="container-fluid">
             <Basic space="space-xs-50 space-lg-80">
                 <header className="text-center">
-                    <h1>Home</h1>
+                    <h1>Lead</h1>
                 </header>
+
+                <section>
+                    <FormLead />
+                </section>
             </Basic>
         </div>
     </main>
---
+++ src/js/react/form/FormLead.js
@@ -0,0 +1,118 @@
+import React, { Component, createRef } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import PropTypes from 'prop-types';
+import * as actionAccount from '../../redux/action/actionAccount';
+import { ACCOUNT_REGISTER_REQUEST } from '../../redux/type';
+import { findByString, removeStatus } from '../../filter';
+import InputButton from '../input/InputButton';
+import InputText from '../input/InputText';
+
+class FormLead extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            form: {},
+            error: {},
+        };
+        this.isFocus = createRef();
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+    componentDidMount() {
+        this.isFocus.current.focus();
+    }
+    onChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const field = target.name;
+        const group = target.dataset.group;
+        const form = group
+            ? {
+                  ...this.state.form,
+                  [group]: {
+                      ...this.state.form[group],
+                      [field]: value,
+                  },
+              }
+            : {
+                  ...this.state.form,
+                  [field]: value,
+              };
+        this.setState({
+            form,
+        });
+    }
+    onSubmit(event) {
+        const { actionAccount } = this.props;
+        const { form } = this.state;
+        event.preventDefault();
+        this.isValid() && actionAccount.accountRegister(form);
+    }
+    isValid() {
+        const { form } = this.state;
+        const error = {};
+        const emailLength = 5;
+        let valid = true;
+        (form.email === undefined || form.email.length < emailLength) &&
+            (error.email = `Email must be at least ${emailLength} characters.`) &&
+            (valid = false);
+        this.setState({
+            error,
+        });
+        return valid;
+    }
+    render() {
+        const { submitting } = this.props;
+        const { form, error } = this.state;
+        const size = 'lg';
+        return (
+            <form id="form-lead" className={`form form-${size} mx-lg-auto`} onSubmit={this.onSubmit}>
+                <InputText
+                    type="email"
+                    name="email"
+                    label="Email"
+                    placeholder="Email"
+                    size={size}
+                    onChange={this.onChange}
+                    value={form.email}
+                    error={error.email}
+                    reference={this.isFocus}
+                />
+                <div className="form-group">
+                    <InputButton
+                        type="submit"
+                        name="register"
+                        label={submitting ? 'Submitting...' : 'Submit'}
+                        kind="success"
+                        size={size}
+                        display="block"
+                        disabled={submitting}
+                    />
+                </div>
+            </form>
+        );
+    }
+}
+
+FormLead.propTypes = {
+    submitting: PropTypes.bool.isRequired,
+    actionAccount: PropTypes.objectOf(PropTypes.func).isRequired,
+};
+
+function mapStateToProps({ calls }) {
+    return {
+        submitting: findByString(calls, removeStatus(ACCOUNT_REGISTER_REQUEST)),
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actionAccount: bindActionCreators(actionAccount, dispatch),
+    };
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(FormLead);
