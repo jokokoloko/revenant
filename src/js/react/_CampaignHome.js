1dca7fecba80ab928bca3e076bbc7cf46f7c962a
jokokoloko <jhowland82@gmail.com>
2018-10-22 16:54:16 -0700

    Added usersMap to _CampaignHome.js

--- src/js/react/_CampaignHome.js
+++ src/js/react/_CampaignHome.js
@@ -3,32 +3,38 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import PropTypes from 'prop-types';
+import * as actionUser from '../redux/action/actionUser';
 import * as actionCampaign from '../redux/action/actionCampaign';
-import { CAMPAIGNS_LOAD_REQUEST } from '../redux/type';
+import { USERS_LOAD_REQUEST, CAMPAIGNS_LOAD_REQUEST } from '../redux/type';
 import { findByString, removeStatus } from '../filter';
+import { arrayToObject } from '../function';
+import * as logic from '../logic';
 import * as path from '../path';
 import Basic from './section/Basic';
 import Loader from './unit/Loader';
 
 class _CampaignHome extends Component {
     componentDidMount() {
-        const { actionCampaign } = this.props;
+        const { actionUser, actionCampaign } = this.props;
+        actionUser.usersLoad(true);
         actionCampaign.campaignsLoad(true);
     }
     render() {
-        const { match, loadingCampaigns, profile, campaigns } = this.props;
+        const { match, loadingUsers, loadingCampaigns, profile, campaigns, usersMap } = this.props;
         const item = 'campaign';
         const empty = '-';
         const labelCampaign = ['Title', 'Medium', 'Author', 'Action'];
         const loopCampaign = campaigns.map((campaign, index) => {
             const count = campaigns.length - index;
+            const user = usersMap[campaign.user] || {};
+            const campaignAuthor = logic.userNameHandleEmail(user, empty);
             return (
                 <tr key={campaign.id} id={campaign.id} className={`${item} ${item}-${count}`}>
                     <th className={`${item}-title`} scope="row">
                         {campaign.title || empty}
                     </th>
                     <td className={`${item}-medium`}>{campaign.medium || empty}</td>
-                    <td className={`${item}-user`}>{campaign.user || empty}</td>
+                    <td className={`${item}-user`}>{campaignAuthor}</td>
                     <td className={`${item}-action`}>
                         <Link to={`/${campaign.id}`}>View</Link>
                         {profile.id === campaign.user && (
@@ -54,7 +60,7 @@ class _CampaignHome extends Component {
                         </header>
 
                         <section className="node-xs-50">
-                            {loadingCampaigns ? (
+                            {loadingUsers || loadingCampaigns ? (
                                 <Loader position="exact-center fixed" label="Loading campaigns" />
                             ) : (
                                 <div className="table-container table-responsive-sm">
@@ -97,22 +103,28 @@ class _CampaignHome extends Component {
 
 _CampaignHome.propTypes = {
     match: PropTypes.objectOf(PropTypes.any).isRequired,
+    loadingUsers: PropTypes.bool.isRequired,
     loadingCampaigns: PropTypes.bool.isRequired,
     profile: PropTypes.objectOf(PropTypes.any).isRequired,
     campaigns: PropTypes.arrayOf(PropTypes.object).isRequired,
+    actionUser: PropTypes.objectOf(PropTypes.func).isRequired,
     actionCampaign: PropTypes.objectOf(PropTypes.func).isRequired,
 };
 
-function mapStateToProps({ profile, calls, campaigns }) {
+function mapStateToProps({ profile, calls, users, campaigns }) {
+    const usersMap = arrayToObject(users, 'id');
     return {
+        loadingUsers: findByString(calls, removeStatus(USERS_LOAD_REQUEST)),
         loadingCampaigns: findByString(calls, removeStatus(CAMPAIGNS_LOAD_REQUEST)),
         profile,
         campaigns,
+        usersMap,
     };
 }
 
 function mapDispatchToProps(dispatch) {
     return {
+        actionUser: bindActionCreators(actionUser, dispatch),
         actionCampaign: bindActionCreators(actionCampaign, dispatch),
     };
 }
